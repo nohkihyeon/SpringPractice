dc1bc73f4c548c40797cc7c10aa51d96ac002589
nohkihyeon <hae8573@koreatech.ac.kr>
2022-02-04 18:00:17 +0900

    mybatis

---
+++ demo/src/main/java/com/example/demo/dto/JsonTestRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class JsonTestRequest {
+	private String name;
+	
+	public JsonTestRequest() {
+		
+	}
+	
+	public JsonTestRequest(String name) {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
